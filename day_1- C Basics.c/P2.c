dd4ed9df39a05ddf70048a81619ffecf0c017e86
unknown <kirthika>
2026-06-19 22:49:34 +0530

    Day 1: 10 programs practiced

---
+++ day_1- C Basics.c/P2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() 
+{
+   int a,mul;
+   printf("enter number:");
+   scanf("%d",&a);
+   mul=a*5;
+   printf("Result:%d",mul);
+   return 0;
+}
---
+++ day_1- C Basics.c/P4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() 
+{
+   int a,mod;
+   printf("Enter a number:");
+   scanf("%d",&a);
+   mod=a%8;
+   printf("Result:%d",mod);
+}
